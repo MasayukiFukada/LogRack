916449b737cfda67935433c1d23664275ffef2b2
MasayukiFukada <m.fukada0813@gmail.com>
2020-08-16 22:06:15 +0900

    Mutation のエラーハンドリングを追加

--- lib/log_lac_web/resolvers/device_status_resolvers.ex
+++ lib/log_lac_web/resolvers/device_status_resolvers.ex
@@ -2,17 +2,22 @@ defmodule LogLacWeb.Resolvers.DeviceStatusResolvers do
   import LogLac.Member
 
   def create_device_status(_parent, args, _info) do
-    case LogLac.Measurement.create_device_status(args) do
-      {:ok, device_status} ->
-        {:ok, %{
-          :cpu_use_rate => device_status.cpu_use_rate,
-          :memory_use_rate => device_status.memory_use_rate,
-          :storage => device_status.storage,
-          :temperature => device_status.temperature,
-          :wake_on_at => device_status.wake_on_at,
-          :device => find_device_by(device_status.device_code),
-        }}
-      _error ->
+    try do
+      case LogLac.Measurement.create_device_status(args) do
+        {:ok, device_status} ->
+          {:ok, %{
+            :cpu_use_rate => device_status.cpu_use_rate,
+            :memory_use_rate => device_status.memory_use_rate,
+            :storage => device_status.storage,
+            :temperature => device_status.temperature,
+            :wake_on_at => device_status.wake_on_at,
+            :device => find_device_by(device_status.device_code),
+          }}
+        _ ->
+          {:error, "デバイス状態の登録に失敗しました"}
+      end
+    rescue
+      _ ->
         {:error, "デバイス状態の登録に失敗しました"}
     end
   end
--- lib/log_lac_web/resolvers/temperature_resolvers.ex
+++ lib/log_lac_web/resolvers/temperature_resolvers.ex
@@ -2,17 +2,22 @@ defmodule LogLacWeb.Resolvers.TemperatureResolvers do
   import LogLac.Member
 
   def create_temperature(_parent, args, _info) do
-    case LogLac.Measurement.create_temperature(args) do
-      {:ok, temperature} ->
-        {:ok, %{
-          :date => temperature.date,
-          :value => temperature.value,
-          :device => find_device_by(temperature.device_code),
-          :sensor => find_sensor_by(temperature.sensor_code),
-          :inserted_at => temperature.inserted_at,
-          :updated_at => temperature.updated_at
-        }}
-      _error ->
+    try do
+      case LogLac.Measurement.create_temperature(args) do
+        {:ok, temperature} ->
+          {:ok, %{
+            :date => temperature.date,
+            :value => temperature.value,
+            :device => find_device_by(temperature.device_code),
+            :sensor => find_sensor_by(temperature.sensor_code),
+            :inserted_at => temperature.inserted_at,
+            :updated_at => temperature.updated_at
+          }}
+        _ ->
+          {:error, "気温の登録に失敗しました"}
+      end
+    rescue
+      _ ->
         {:error, "気温の登録に失敗しました"}
     end
   end
